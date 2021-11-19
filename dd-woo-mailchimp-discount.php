bf8888833928c6be60091649e9c0ef858314e628
Danil <danilderevyanchenko@gmail.com>
2021-11-20 02:22:10 +0300

    add basic OOP and finish mailchimp api integration

--- dd-woo-mailchimp-discount.php
+++ dd-woo-mailchimp-discount.php
@@ -18,111 +18,151 @@ if( ! defined('ABSPATH') ) {
  * WooCommerce discount Mailchimp
  *****************************************/
 
-/**
- * 1. genetate random coupon name
- */
-function generate_random_coupon_name() {
-	$characters = "ABCDEFGHJKMNPQRSTUVWXYZ23456789";
-	$char_length = "8";
-	$random_string = substr( str_shuffle( $characters ),  0, $char_length );
 
-	return $random_string;
-}
+if ( ! class_exists( 'ddWooMD' ) ) {
+	
+	class ddWooMD
+	{
 
-/**
- * 2. check if coupon_exists
- */
-function coupon_exists($coupon_code) {
-	global $wpdb;
+		public function __construct() 
+		{
+			// add_action( 'init', [$this, 'mailchimp_integration'] );
+		}
 
-	$sql = $wpdb->prepare( "SELECT post_name FROM $wpdb->posts WHERE post_type = 'shop_coupon' AND post_name = '%s'", $coupon_code );
+		/**
+		 * Genetate random coupon name
+		 */
+		public function generate_random_coupon_name() {
+			$characters = "ABCDEFGHJKMNPQRSTUVWXYZ23456789";
+			$char_length = "8";
+			$random_string = substr( str_shuffle( $characters ),  0, $char_length );
 
-	$coupon_codes = $wpdb->get_results($sql);
+			return $random_string;
+		}
 
-	if (count($coupon_codes)> 0) {
-		return true;
+		/**
+		 * Check if coupon_exists
+		 */
+		public function coupon_exists($coupon_code) {
+			global $wpdb;
+
+			$sql = $wpdb->prepare( "SELECT post_name FROM $wpdb->posts WHERE post_type = 'shop_coupon' AND post_name = '%s'", $coupon_code );
+			$coupon_codes = $wpdb->get_results($sql);
+
+			if ( count( $coupon_codes ) > 0) {
+				return true;
+			} else return false;
+		}
+
+		/**
+		 * Create a coupon programatically
+		 */
+		public function dd_create_coupon_code_programmatically() {
+
+			$options = get_option('dd_settings_options');
+			$coupon_code = $this->generate_random_coupon_name();
+
+			if ( $this->coupon_exists( $coupon_code ) ) {
+				$coupon_code = $this->generate_random_coupon_name();
+			} 
+
+			$amount = $options['amount_field']; 
+			$discount_type = $options['discount_type_field']; 
+
+			$coupon = array(
+				'post_title' => $coupon_code,
+				'post_content' => '',
+				'post_status' => 'publish',
+				'post_author' => 1,
+				'post_type' => 'shop_coupon'
+			);
+			$new_coupon_id = wp_insert_post( $coupon );
+
+			update_post_meta( $new_coupon_id, 'discount_type', $discount_type );
+			update_post_meta( $new_coupon_id, 'coupon_amount', $amount );
+			update_post_meta( $new_coupon_id, 'individual_use', 'no' );
+			update_post_meta( $new_coupon_id, 'product_ids', '' );
+			update_post_meta( $new_coupon_id, 'exclude_product_ids', '' );
+			update_post_meta( $new_coupon_id, 'usage_limit', '1' );
+			update_post_meta( $new_coupon_id, 'expiry_date', '' );
+			update_post_meta( $new_coupon_id, 'apply_before_tax', 'yes' );
+			update_post_meta( $new_coupon_id, 'free_shipping', 'no' );
+
+			return $coupon_code;
+		}
+
+		/**
+		 * Send mail with coupon code for the customer 
+		 */
+		public function send_mail_with_coupon_code($subscriber_email_address, $coupon_code) {
+
+			$options = get_option('dd_settings_options');
+			$to = $subscriber_email_address;
+			$subject = $options['mail_title_field'];  // 'Your coupon discount code.'
+			$body =  wpautop( stripslashes ( $options['mail_text_field'] ) ) . '<br><h3>' . $coupon_code . '</h3>'; 
+			$body = htmlspecialchars_decode($body);
+			$mail_headers = "MIME-Version: 1.0" . "\r\n";
+			$mail_headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+			$mailResult = mail($to, $subject, $body, $mail_headers);
+			
+			if( $mailResult ) $mail_response = 'success';
+
+			return array(
+				'coupon_code' => $coupon_code,
+				'email_response' => $mail_response
+			);
+
+		}
+
+		/*
+		 * Create a coupon programatically after user subscribed in mailchimp
+		 */
+		public function mailchimp_integration() 
+		{
+			$email = 'danildere111222444@gmail.com';
+			$list_id = 'c6f33b8965';
+			$api_key = '33685714655b1bcae57573a0e91660d8-us5';
+
+			$data_center = substr($api_key,strpos($api_key,'-') + 1);
+ 
+			$url = 'https://'. $data_center .'.api.mailchimp.com/3.0/lists/'. $list_id .'/members';
+			
+			$json = json_encode([
+				'email_address' => $email,
+				'status'        => 'subscribed', //pass 'subscribed' or 'pending'
+			]);
+			
+			try {
+				$ch = curl_init($url);
+				curl_setopt($ch, CURLOPT_USERPWD, 'user:' . $api_key);
+				curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+				curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+				curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+				curl_setopt($ch, CURLOPT_POST, 1);
+				curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+				curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
+				$result = curl_exec($ch);
+				$status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+				curl_close($ch);
+			
+				if (200 == $status_code) {
+					echo "The user added successfully to the MailChimp. <br><br>";
+					$coupon_code = $this->dd_create_coupon_code_programmatically();
+					$mail_result_array = $this->send_mail_with_coupon_code($email, $coupon_code);
+					var_dump($mail_result_array);
+					wp_die();
+				}
+			} catch(Exception $e) {
+				echo $e->getMessage();
+			}
+		}
+
 	}
-	else {
-		return false;
-	}
+
 }
 
-/**
- * 3. Create a coupon programatically
- */
-function dd_create_coupon_code_programmatically() {
-	$options = get_option('dd_settings_options');
-	$coupon_code = generate_random_coupon_name();
-
-	if ( coupon_exists( $coupon_code ) ) {
-		$coupon_code = generate_random_coupon_name();
-	} 
-
-	$amount = $options['amount_field']; 
-	$discount_type = $options['discount_type_field']; 
-
-	$coupon = array(
-		'post_title' => $coupon_code,
-		'post_content' => '',
-		'post_status' => 'publish',
-		'post_author' => 1,
-		'post_type' => 'shop_coupon'
-	);
-	$new_coupon_id = wp_insert_post( $coupon );
-
-	update_post_meta( $new_coupon_id, 'discount_type', $discount_type );
-	update_post_meta( $new_coupon_id, 'coupon_amount', $amount );
-	update_post_meta( $new_coupon_id, 'individual_use', 'no' );
-	update_post_meta( $new_coupon_id, 'product_ids', '' );
-	update_post_meta( $new_coupon_id, 'exclude_product_ids', '' );
-	update_post_meta( $new_coupon_id, 'usage_limit', '1' );
-	update_post_meta( $new_coupon_id, 'expiry_date', '' );
-	update_post_meta( $new_coupon_id, 'apply_before_tax', 'yes' );
-	update_post_meta( $new_coupon_id, 'free_shipping', 'no' );
-	
-	return $coupon_code;
- };
-
-/**
- * 4. send mail with coupon code for the customer 
- */
-function send_mail_with_coupon_code($subscriber_email_address, $coupon_code) {
-
-	$options = get_option('dd_settings_options');
-	$to = $subscriber_email_address;
-	$subject = $options['mail_title_field'];  // 'Your coupon discount code.'
-	// $body = '<p>Hi There,
-	// 	Thanks for signing up for our Newsletter. We have just created a coupon for you. The coupon code to redeem the discount is</p> 
-	// 	<h3>' . $coupon_code . '</h3>';
-
-	$body =  wpautop( stripslashes ( $options['mail_text_field'] ) ) . '<br><h3>' . $coupon_code . '</h3>'; 
-	$body = htmlspecialchars_decode($body);
-	$mail_headers = "MIME-Version: 1.0" . "\r\n";
-	$mail_headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-
-	$mailResult = mail($to, $subject, $body, $mail_headers);
-	
-	if( $mailResult )
-		$mail_response = 'success';
-
-	return array(
-	'coupon_code' => $coupon_code,
-	'email_response' => $mail_response
-	);
-}
-
-
-/***********************************************************************
-* Create a coupon programatically after user subscribed in mailchimp
-*/
-add_action( 'mc4wp_form_subscribed', 'dd_action_mc4wp_form_subscribed', 10, 4 ); 
-function dd_action_mc4wp_form_subscribed( $form, $subscriber_email_address, $data, $map ) { 
-	
-	$coupon_code = dd_create_coupon_code_programmatically();
-	$mail_result_array = send_mail_with_coupon_code($subscriber_email_address, $coupon_code);
-
-	// var_dump($mail_result_array);
-}; 
+$ddWooMD = new ddWooMD();
 
 
 /**
@@ -264,15 +304,6 @@ function mail_text_field_html() {
 }
 
 function main_admin_page_callback() {
-	/**
-	 * Must have needed chenges fields:
-	 * - $amount (10%..)
-	 * - $discount_type select - fixed_cart, percent, fixed_product, percent_product
-	 * 
-	 */
-	
-	// $options = get_option('dd_settings_options');
-	// var_dump( $options );
 	?>
 	<div class="content dd_settings_page_content">
 		<?php settings_errors(); ?>
